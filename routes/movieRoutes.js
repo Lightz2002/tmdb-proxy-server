25b54a3ed4a6cc7eacb2c21ae427e86264d03bce
ezb_ryankenidy <kenidyryan@gmail.com>
2023-10-02 19:38:06 +0700

    add top rated movie

--- routes/movieRoutes.js
+++ routes/movieRoutes.js
@@ -28,10 +28,32 @@ router.get("/popular", async (req, res) => {
 
 router.get("/now_playing", async (req, res) => {
   try {
+    const { language, page } = req.query;
     // Forward the request to TMDb API
     const response = await axios.get(
-      `https://api.themoviedb.org/3/movie/now_playing
-    ?language=en-US&page=1`,
+      `https://api.themoviedb.org/3/movie/now_playing?language=${language}&page=${page}`,
+      {
+        headers: {
+          Authorization: token,
+          // You can add other query parameters as needed
+        },
+      }
+    );
+
+    // Send the TMDb API response to the client
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+router.get("/top_rated", async (req, res) => {
+  try {
+    const { language, page } = req.query;
+    // Forward the request to TMDb API
+    const response = await axios.get(
+      `https://api.themoviedb.org/3/movie/top_rated?language=${language}&page=${page}`,
       {
         headers: {
           Authorization: token,
